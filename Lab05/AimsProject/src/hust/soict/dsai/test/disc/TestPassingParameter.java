df06680c1debdbaea57488e1810130a4a21d1bce
hoangtienlong642 <85188594+hoangtienlong642@users.noreply.github.com>
2023-01-06 21:13:24 +0700

    Add files via upload

---
+++ Lab05/AimsProject/src/hust/soict/dsai/test/disc/TestPassingParameter.java
@@ -0,0 +1,42 @@
+package hust.soict.dsai.test.disc;
+
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+public class TestPassingParameter {
+	public static void swap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+		DigitalVideoDisc tmp = new DigitalVideoDisc(dvd1);
+		dvd1.setTitle(dvd2.getTitle());
+		dvd1.setCategory(dvd2.getCategory());
+		dvd1.setDirector(dvd2.getDirector());
+		dvd1.setLength(dvd2.getLength());
+		dvd1.setCost(dvd2.getCost());
+
+		dvd2.setTitle(tmp.getTitle());
+		dvd2.setCategory(tmp.getCategory());
+		dvd2.setDirector(tmp.getDirector());
+		dvd2.setLength(tmp.getLength());
+		dvd2.setCost(tmp.getCost());
+	}
+//	public static void swap(Object o1, Object o2) {
+//		Object tmp = o1;
+//		o1 = o2;
+//		o2 = tmp;
+//	}
+	public static void changeTitle(DigitalVideoDisc dvd, String title) {
+		String oldTitle = dvd.getTitle();
+		dvd.setTitle(title);
+		dvd = new DigitalVideoDisc(oldTitle);
+	}
+	public static void main(String[] args) {
+		DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+		DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+		swap(jungleDVD, cinderellaDVD);
+
+		System.out.println("Jungle dvd title: " + jungleDVD.getTitle());
+		System.out.println("Cinderella dvd title: " + cinderellaDVD.getTitle());
+
+		changeTitle(jungleDVD, cinderellaDVD.getTitle());
+		System.out.println("Jungle dvd title: " + jungleDVD.getTitle());
+	}
+}
